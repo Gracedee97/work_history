c0e3a3278d4494afcf614126a2162deddefc8cf6
Gracedee <gracedee97@gmail.com>
2018-01-28 21:28:45 +0100

    dog years

---
+++ week_4_Grace/dog-years.js
@@ -0,0 +1,13 @@
+const myAge = 24;
+// i am 24 years old.
+let earlyYears = 2;
+// human years of dog's life.
+earlyYears *= 10.5;
+laterYears = myAge - 2;
+// later years is equal to myAge minus 2.
+laterYears *= 4;
+// the later years multiplied by 4.
+myAgeInDogYears = earlyYears + laterYears;
+let myName = "Grace"
+"Grace".toLowerCase();
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years`);
